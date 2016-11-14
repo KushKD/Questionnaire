a5fe4e5af9ddbc97f69e30b06531aa903471f96e
kushkd <kushkumardhawan@gmail.com>
2016-11-14 15:10:10 +0530

    -- Forms Ready --Working on Database

--- app/src/main/java/questionnaire/himachal/dit/questionnaire/MainActivity.java
+++ app/src/main/java/questionnaire/himachal/dit/questionnaire/MainActivity.java
@@ -1,14 +1,20 @@
 package questionnaire.himachal.dit.questionnaire;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import Model.QuestionsData;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton radioSexButton;
     LinearLayout layout;
     EditText answertwo,answerthree;
+
+    QuestionsData QD = new QuestionsData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +39,25 @@ public class MainActivity extends AppCompatActivity {
         answertwo = (EditText)findViewById(R.id.answertwo);
         answerthree = (EditText)findViewById(R.id.answerthree);
 
-        radiochoice_RG.setOnClickListener(new View.OnClickListener() {
-
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiochoice);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                // get selected radio button from radioGroup
                 int selectedId = radiochoice_RG.getCheckedRadioButtonId();
-
-                // find the radiobutton by returned id
                 radioSexButton = (RadioButton) findViewById(selectedId);
-
-                Toast.makeText(getApplicationContext(), radioSexButton.getText(), Toast.LENGTH_SHORT).show();
+                QD.setQuestion_One(radioSexButton.getText().toString().trim());
 
                 if(radioSexButton.getText().toString().equalsIgnoreCase("Yes")){
                     layout.setVisibility(View.VISIBLE);
                 }else{
                     layout.setVisibility(View.GONE);
                 }
-
             }
-
         });
+
+
 
         Clear_bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +76,46 @@ public class MainActivity extends AppCompatActivity {
         proceed_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //// TODO: 11/14/2016  
+
+                QD.setQuestion_Two(answertwo.getText().toString().trim());
+                QD.setQuestion_Three(answerthree.getText().toString().trim());
+
+                ShowAlert(QD);
+
+
             }
         });
     }
+
+    private void ShowAlert(QuestionsData  s) {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.dialog_info);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(lp);
+        dialog.setTitle("Feedback");
+        dialog.setCancelable(false);
+        dialog.show();
+        TextView DialogInfo = (TextView)dialog.findViewById(R.id.dialog_info);
+        DialogInfo.setText("Contact Information");
+        Button agree = (Button)dialog.findViewById(R.id.dialog_ok);
+        Button dialog_exit = (Button)dialog.findViewById(R.id.dialog_exit);
+
+        dialog_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+            }
+        });
+        agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 }
